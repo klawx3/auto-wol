62b4e857372d85528006ffb07d0b7a2780b9b1da
klawx3 <klawx3@gmail.com>
2021-02-02 17:09:31 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from icmplib import ping
+from time import sleep
+from pywol import wake
+from os import environ
+
+TIMEOUT = environ['TIMEOUT']
+HOST = environ['HOST']
+MAC  = environ['MAC']
+BROADCAST = environ['BROADCAST']
+
+while True:
+    host = ping(HOST,count=1,interval=1,timeout=TIMEOUT)
+    sleep(TIMEOUT)
+    if host.is_alive:
+        print("Host:{}\tRTT avg:{}\tPacket Loss:{}".format(host.address,host.avg_rtt,host.packet_loss))
+    else:
+        print("Host:{} Death... sending WOL -> MAC: {}\tBROADCAST:{} ".format(HOST,MAC,BROADCAST))
+        wake(MAC,ip_address=BROADCAST)
